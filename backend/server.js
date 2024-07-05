e157a9ffe0ef04bf29e3b7e57cbfef3eb1f401b8
shanusen27 <shashinianuththara20@gmail.com>
2024-07-05 23:40:41 +0530

    backend basic

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routs.js";
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
+import { app, server } from "./socket/socket.js";
+import cors from "cors";
+
+
+const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
+
+dotenv.config();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes);
+app.use('/api/users',userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist)));
+
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
+
+//app.get('/',function(req,res){
+//    res.send("Hello World!");
+//)});
+
+server.listen(PORT,function(){
+    connectToMongoDB();
+    console.log("Server is running " + `${PORT}`);
+});
